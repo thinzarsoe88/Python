499ccef64d6cbc722f92e4dc68e1b183369be944
thinzarsoe88 <108972594+thinzarsoe88@users.noreply.github.com>
2023-01-08 16:06:19 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -5,3 +5,8 @@ famous_person = input("Famous person: ")
 
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \n I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
 print(madlib)
+
+
+
+
+
