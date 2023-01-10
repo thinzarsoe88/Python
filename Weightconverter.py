13210f63930f6cc6e7c4b7993307112ceba20a20
thinzarsoe88 <108972594+thinzarsoe88@users.noreply.github.com>
2023-01-09 17:35:45 -0800

    Add files via upload

---
+++ Weightconverter.py
@@ -0,0 +1,12 @@
+weight = float(input("Enter your weight: "))
+unit = input("Kilograms or Pounds? (K or L): ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "Lbs."
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "Kgs."
+else:
+    print(f"{unit} was not valid")
+print(f"Your weight is: {weight} {unit}")
